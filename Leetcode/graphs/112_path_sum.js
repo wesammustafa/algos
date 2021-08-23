6e0465b62cf59441160b9bec2d3d7d66c2cdc6f5
wesam mustafa <wesam.mustafa@mo4group.com>
2021-08-23 23:05:51 +0200

    Finish #112 path sum problem

---
+++ Leetcode/graphs/112_path_sum.js
@@ -0,0 +1,14 @@
+var hasPathSum = function (root, targetSum) {
+  if (root === null) return false;
+  else if (
+    root.left === null &&
+    root.right === null &&
+    targetSum - root.val === 0
+  )
+    return true;
+  else
+    return (
+      hasPathSum(root.left, targetSum - root.val) ||
+      hasPathSum(root.right, targetSum - root.val)
+    );
+};
