189054d68d7b9375ad4e227a854cafe1731f6166
wesam mustafa <wesam.mustafa100@gmail.com>
2021-08-29 23:13:46 +0200

    Finish #9 Palindrome Number

---
+++ Leetcode/easy/9. Palindrome Number.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  let c = x;
+  let b = 0;
+
+  if (x < 0) {
+    return false;
+  }
+
+  while (c) {
+    b = b * 10 + (c % 10);
+    c = Math.floor(c / 10);
+  }
+
+  return b === x;
+};
+
+//------------------------------------------------------------
+//second solution
+var isPalindrome = function (x) {
+  if (x < 0 || (x % 10 === 0 && x != 0)) {
+    return false;
+  }
+
+  let b = 0;
+  while (x > b) {
+    b = b * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+
+  return x === b || x === b / 10;
+};
