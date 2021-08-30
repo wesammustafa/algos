52f82dddbe1a38075b33178bc76c5aa5c4cb379d
wesam mustafa <wesam.mustafa100@gmail.com>
2021-08-30 21:25:29 +0200

    Finish #1 Two Sum Problem

---
+++ Leetcode/easy/1. Two Sum.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// time complexity O(n^2)
+// space complexity O(n)
+// brute force solution
+var twoSum = function (nums, target) {
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+
+  return result;
+};
+
+// time complexity O(n)
+// space complexity O(n)
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i];
+    if (hash[target - n] !== undefined) {
+      return [hash[target - n], i];
+    }
+    hash[n] = i;
+  }
+  return [];
+};
