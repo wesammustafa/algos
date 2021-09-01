d2b5d4f3ed8ab8b29f312c58a2984ca2c3c2c529
wesam mustafa <wesam.mustafa100@gmail.com>
2021-09-02 00:44:49 +0200

    Another solution for #739 Daily Temperatures

--- Leetcode/stack/739. Daily Temperatures.js
+++ Leetcode/stack/739. Daily Temperatures.js
@@ -18,3 +18,27 @@
     
     return result;
 };
+
+
+// time complexity: O(N)
+// space complexity: O(N)
+
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+ var dailyTemperatures = function(temperatures) {
+    let result = new Array(temperatures.length).fill(0);
+    let stack = []; //will contain [temp, index]
+    
+    for (let i = 0; i <= temperatures.length - 1; i++) {
+        while(stack.length > 0 && temperatures[i] > stack[stack.length - 1 ][0]) {
+            const [temp, index] = stack.pop();
+            result[index] = i - index;
+        }
+        stack.push([temperatures[i], i])
+    }
+    
+    
+    return result;
+};
