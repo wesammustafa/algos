b1784113c63fb652976e33892d736d227d8bc3ed
wesam mustafa <wesam.mustafa100@gmail.com>
2021-09-08 23:52:05 +0200

    Finish #394 Decode String

---
+++ Leetcode/stack & dfs/394. Decode String.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var decodeString = function(s) {
+    const stack = [];
+    
+    for (let letter of s) {
+        if (letter != "]") {
+            stack.push(letter);
+        } else {
+            let substr = "";
+            while (stack[stack.length - 1] != "[")
+                substr = stack.pop() + substr;
+            
+            // remove closing open bracket
+            stack.pop();
+            
+            let k = "";
+            
+            while (stack.length && /^\d+$/.test(stack[stack.length - 1])) 
+                k = stack.pop() + k;
+
+            stack.push(substr.repeat(Number(k)))            
+        }
+    }
+    
+    return stack.join("");
+};
