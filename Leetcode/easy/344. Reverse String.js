68d86f8997901f9f1bfa838fee04829c26dd1508
wesam mustafa <wesam.mustafa100@gmail.com>
2021-09-11 23:32:26 +0200

    Add another solution for #344 Reverse String

--- Leetcode/easy/344. Reverse String.js
+++ Leetcode/easy/344. Reverse String.js
@@ -5,8 +5,25 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
- var reverseString = (s) => {
-    for (let [a, b] = [0, s.length - 1]; a < b; a++, b--) {
-        [s[a], s[b]] = [s[b], s[a]]
+var reverseString = (s) => {
+  for (let [a, b] = [0, s.length - 1]; a < b; a++, b--) {
+    [s[a], s[b]] = [s[b], s[a]];
+  }
+};
+
+//------------------------------------------------------------------------------------------------------------------
+// Recursive solution using the opposite directional two-pointer approach
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = (s) => {
+  let helper = (left, right) => {
+    if (left < right) {
+      [s[left], s[right]] = [s[right], s[left]];
+      helper(left + 1, right - 1);
     }
+  };
+
+  helper(0, s.length - 1);
 };
